b4f803a098124a1213458b5f0ed2431c3821ffbd
SuperCaliFragilisticExpialiDociousMan <91343063+SuperCaliFragilisticExpialiDociousMan@users.noreply.github.com>
2022-04-10 16:37:45 -0400

    a9

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import helloController from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
-import tuitsController   from "./controllers/tuits-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
+import mongoose from 'mongoose';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/web-dev'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
